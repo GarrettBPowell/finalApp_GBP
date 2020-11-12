98597c626447a307584dc8aec7dd7633b81a4c5e
Garett Powell <garettbpowell@gmail.com>
2020-11-12 08:42:32 -0600

    Initial Commit

---
+++ finalApp_GBP/finalApp_GBPApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalApp_GBPApp.swift
+//  finalApp_GBP
+//
+//  Created by Garrett Powell on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct finalApp_GBPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
