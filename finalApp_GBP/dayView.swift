212e759640359e2fd19e043b3883c80d41ebfeb3
Garrett Powell <garettbpowell@gmail.com>
2020-11-17 12:00:24 -0600

    I can see a cool looking year view in month format, need to make dates clickable

---
+++ finalApp_GBP/dayView.swift
@@ -0,0 +1,20 @@
+//
+//  dayView.swift
+//  finalApp_GBP
+//
+//  Created by Garrett Powell on 11/17/20.
+//
+
+import SwiftUI
+
+struct dayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct dayView_Previews: PreviewProvider {
+    static var previews: some View {
+        dayView()
+    }
+}
